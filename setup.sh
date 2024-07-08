9b6eb958af61a6fc574345d44ed15ecce18ff99f
Nigel Johnson <nigeljohnson73@users.noreply.github.com>
2024-07-08 11:50:38 +0100

    require GIT

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
-sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel
+sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel git
 
 python -m venv ~/.env
 
