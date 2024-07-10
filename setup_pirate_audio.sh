797353c27e9745c46b9b7b25eff3e9849d72b232
Nigel Johnson <nigeljohnson73@users.noreply.github.com>
2024-07-10 11:32:17 +0100

    Create setup_pirate_audio.sh

---
+++ setup_pirate_audio.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel git 
+
+sudo apt install -y vim
+sudo find /etc/vim/vimrc -exec sed -i 's/^"syntax on/syntax on/g' '{}' \;
+
+#### BLUETOOTH
+sudo apt install -y python3-dbus 
+python -m venv --system-site-packages ~/.env
+. ~/.env/bin/activate
+pip install bluedot
+
+
+
+#### PIRATE
+sudo sed -i "s/^dtparam=audio=on/#dtparam=audio=on/g" /boot/firmware/config.txt
+sudo bash -c 'cat >> /boot/firmware/config.txt' <<EOF
+dtoverlay=hifiberry-dac
+gpio=25=op,dh
+EOF
+sudo apt remove -y python3-rpi.gpio && sudo apt autoremove -y
+sudo apt install -y python3-lgpio libopenblas-dev libopenjp2-7
+python -m venv --system-site-packages ~/.env
+pip install numpy pillow spidev st7789 rpi-lgpio
+git clone https://github.com/pimoroni/pirate-audio
+python pirate-audio/examples/rainbow.py
+python pirate-audio/examples/buttons.py
+speaker-test -c2 -twav -l2
+
+
+
+#### NEOPIXEL
+pip install rpi_ws281x adafruit-circuitpython-neopixel
+pip install --force-reinstall adafruit-blinka
+
