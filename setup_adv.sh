098ec87ce9771d690478ccd5593c71ecc54610f6
Nigel <nigel@nigeljohnson.net>
2024-07-11 21:56:04 +0100

    Moved advanced setup about.

--- setup_adv.sh
+++ setup_adv.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+#### Combo
+sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+sudo raspi-config nonint do_spi 0
+sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel libopenblas-dev git vim python3-dbus libopenblas-dev libopenjp2-7 fonts-dejavu
+sudo apt remove -y python3-rpi.gpio && sudo apt autoremove -y
+sudo apt install -y python3-lgpio
+
+python -m venv --system-site-packages ~/.env
+. ~/.env/bin/activate
+
+pip install bluedot numpy pillow spidev st7789 rpi-lgpio rpi_ws281x adafruit-circuitpython-neopixel
+pip install --force-reinstall adafruit-blinka
+git clone https://github.com/nigeljohnson73/neoclock.git
+
+
+############################################################################
+
 #### Basic config for python env
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y
 sudo apt install -y python3-dev python3-pip python3-setuptools python3-wheel git 
@@ -36,6 +53,9 @@ python pirate-audio/examples/rainbow.py
 python pirate-audio/examples/buttons.py
 speaker-test -c2 -twav -l2
 
+
+#### inky
+# https://www.waveshare.com/wiki/2.13inch_e-Paper_HAT_Manual#Overview
 
 
 #### NEOCLOCK
