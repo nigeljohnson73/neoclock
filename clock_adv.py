3c8091525b399ffc0ae3b2c21017cfa125cc33d5
Nigel <nigel@nigeljohnson.net>
2024-07-11 23:02:07 +0100

    Added buttons for WaveShare

--- clock_adv.py
+++ clock_adv.py
@@ -6,7 +6,7 @@ import neopixel
 from colorsys import hsv_to_rgb
 from PIL import Image, ImageDraw, ImageFont
 from st7789 import ST7789
-#from adafruit_rgb_display import st7789
+from adafruit_rgb_display import st7789
 
 
 #################
@@ -49,10 +49,21 @@ class NjButton():
             print(f"Button {self.label}: change state to {not self.state}")
         self.last_state = self.state
 
+# Pirate Audio board
 buttons = [ NjButton(board.D5, "A"),
             NjButton(board.D6, "B"),
             NjButton(board.D16, "X"),
             NjButton(board.D24, "Y"),
+          ]
+
+ws_buttons = [ NjButton(board.D21, "KEY1"),
+               NjButton(board.D20, "KEY2"),
+               NjButton(board.D16, "KEY3"),
+               NjButton(board.D6, "UP"),
+               NjButton(board.D19, "DOWN"),
+               NjButton(board.D5, "LEFT"),
+               NjButton(board.D26, "RIGHT"),
+               NjButton(board.D13, "PRESS"),
           ]
 
 def setPixel(n, mult, arr):
@@ -90,8 +101,6 @@ def loop():
     hue = (time.time()/10) % 255
     r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
     draw.rectangle((0, 0, 240, 240), (r, g, b))
-    #disp.image(image)
-
     text = "Hello World!"
     text_left, text_top, text_right, text_bottom = draw.textbbox((0,0), text=text, font=font) 
     text_width, text_height = (text_right - text_left, text_bottom - text_top)
