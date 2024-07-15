017041e8eb7b2dad3e3f0a783ce1b63f1fd6bf60
Nigel Johnson <nigel@nigeljohnson.net>
2024-07-15 20:28:31 +0100

    tidyup

--- setup.sh
+++ setup.sh
@@ -18,23 +18,7 @@ sudo cat /etc/rc.local | grep -v 'exit 0' | grep -v 'clock.sh'
 sudo rm /etc/rc.local
 sudo mv /etc/rc.local.tmp /etc/rc.local
 sudo bash -c 'cat >> /etc/rc.local' <<EOF
-# su -l pi -c /hone/pi/neoclock/clock.sh >/tmp/clock.log 2>&1 &
 /home/pi/neoclock/clock.sh >/tmp/clock.log 2>&1 &
 exit 0
 EOF
 sudo chmod 755 /etc/rc.local
-
-# Want syntax highlighting? VIM is the answer
-#sudo apt install -y vim
-#sudo find /etc/vim/vimrc -exec sed -i 's/^"syntax on/syntax on/g' '{}' \;
-
-######################################
-# Stuff here is patched together for the pirate audio hat since the documetation is crap
-#sudo apt remove python3-rpi.gpio
-#sudo apt install -y python3-dev python3-pip python3-setuptools python3-lgpio python3-wheel git libopenblas-dev
-#python -m venv --system-site-packages ~/.env
-#pip install numpy pillow spidev st7789
-#sudo raspi-config nonint do_spi 0
-# Do some DT overlay stuff: https://shop.pimoroni.com/products/pirate-audio-mini-speaker?variant=31189753692243
-# sudo reboot
-#git clone https://github.com/pimoroni/pirate-audio
