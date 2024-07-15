7f32f1328bbc9b0c53c951342143192f33d22ec3
Nigel <nigel@nigeljohnson.net>
2024-07-15 12:48:19 +0100

    Weather Integration

--- _app/EinkDisplay.py
+++ _app/EinkDisplay.py
@@ -4,6 +4,7 @@ import datetime
 from PIL import Image,ImageDraw,ImageFont
 from _app.NjDisplay import NjDisplay
 from _app.KillableThread import KillableThread
+from _app.WeatherApi import getForecast
 
 epd = False
 def setupModule():
@@ -21,8 +22,9 @@ picdir = os.path.dirname(os.path.realpath(__file__))
 #if os.path.exists(libdir):
 #    sys.path.append(libdir)
 
-font72 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 72)
-font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+font_time = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 72)
+font_date = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+font_temp = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 20)
 
 display_updating = False
 def drawEinkDisplay(display, multicolor):
@@ -37,39 +39,71 @@ def drawEinkDisplay(display, multicolor):
     #height = epd.height
     width = epd.height
     height = epd.width
-    print(f"drawEinkDisplay(({width}x{height}), multicolor: {multicolor})")
+    print(f"drawEinkDisplay(dim: ({width}, {height}), multicolor: {multicolor})")
 
     imgb = Image.new('1', (epd.height, epd.width), 255)  # 250*122
-    #imgc = Image.new('1', (epd.height, epd.width), 255)  # 250*122
     imgc = Image.new('1', (epd.height, epd.width), 255) if multicolor else imgb
     drawb = ImageDraw.Draw(imgb)
     drawc = ImageDraw.Draw(imgc) if multicolor else drawb
-
-    #tme = f"{t.hour:02d}:{t.minute:02d}:{t.second:02d}"
 
     t = datetime.datetime.now()
     tme = t.strftime("%H:%M")
     dte = t.strftime("%a %d %B %Y")
 
-    font = font72
+    '''
+    font = font_time
     text_left, text_top, text_right, text_bottom = drawb.textbbox((0,0), text=tme, font=font)
     text_width, text_height = (text_right - text_left, text_bottom - text_top)
     d=(width//2 - text_width//2, height//2 - text_height)
     drawb.text((width//2 - text_width//2, height//2 - text_height-1), tme, font=font, fill=0)
 
     #print(f"dim:({width},{height}), txt: ({text_width},{text_height}), drw:{d}")
-    font = font24
+    '''
+    font = font_date
     text_left, text_top, text_right, text_bottom = drawc.textbbox((0,0), text=dte, font=font)
     text_width, text_height = (text_right - text_left, text_bottom - text_top)
-    drawc.text((width//2 - text_width//2, height//2 + text_height+1), dte, font=font, fill=0)
+    drawc.text((width//2 - text_width//2, height - text_height-5), dte, font=font, fill=0)
+
+    fc = getForecast()
+    forecast_current_img = None
+    forecast_next_img = None
+    if len(fc):
+        forecast_current_img = fc["now"]["condition_img"]
+        forecast_next_img = fc["next"]["condition_img"]
+
+        tmp = f'{round(fc["now"]["temp_c"])}C / {round(fc["now"]["humidity"])}%'
+        font = font_temp
+        cy=64
+        text_left, text_top, text_right, text_bottom = drawb.textbbox((0,0), text=tmp, font=font)
+        text_width, text_height = (text_right - text_left, text_bottom - text_top)
+        tp=(1*width//4-text_width//2, cy)
+        drawb.text(tp, tmp, font=font, fill=0)
+
+        tmp = f'{round(fc["next"]["mintemp_c"])}C / {round(fc["next"]["maxtemp_c"])}C'
+        font = font_temp
+        cy=64
+        text_left, text_top, text_right, text_bottom = drawb.textbbox((0,0), text=tmp, font=font)
+        text_width, text_height = (text_right - text_left, text_bottom - text_top)
+        tp=(3*width//4-text_width//2, cy)
+        drawb.text(tp, tmp, font=font, fill=0)
+
+    if forecast_current_img:
+        x = int(1*imgb.width/4 - forecast_current_img.width/2)
+        y = 0 #(image.height - self.forecast_current_img.height) // 2
+        imgb.paste(forecast_current_img, (x, y), forecast_current_img)
+
+    if forecast_next_img:
+        x = int(3*imgb.width/4 - forecast_next_img.width/2)
+        y = 0 #(image.height - self.forecast_next_img.height) // 2
+        imgb.paste(forecast_next_img, (x, y), forecast_next_img)
 
     drawb.arc((0,0,10,10),  0, 360, fill=0)
     if display.btConnected():
-        print("Display says BT is connected")
+        #print("Display says BT is connected")
         drawc.ellipse((1,1,9,9), fill=0)
-    else:
-        print("Display says BT is DISConnected")
-        pass
+    #else:
+        #print("Display says BT is DISConnected")
+        #pass
 
     epd.display(epd.getbuffer(imgb), epd.getbuffer(imgc))
     epd.sleep()
